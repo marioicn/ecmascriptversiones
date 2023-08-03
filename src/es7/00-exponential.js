b9ec06ed519c2919b00a59440cbaaa00e9bf0ea0
mario <marioicnltda@gmail.com>
2023-08-03 10:26:54 -0500

    ecmascript7

---
+++ src/es7/00-exponential.js
@@ -0,0 +1,4 @@
+const dara = 3 ** 4;
+
+
+console.log(dara)
---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,7 @@
+let numbers =[1,2,3,4,5,6,7,8,9,0]
+
+console.log(numbers.includes(10))
+
+let names =["mario","alejandro"]
+
+console.log(names.includes("mario"))
